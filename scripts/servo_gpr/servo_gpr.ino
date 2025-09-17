bc7728772690abd9fde290714c08e348a39aba13
andrew-dave <evadaven.andrew@gmail.com>
2025-09-17 16:55:09 -0400

    Record+stream-gst v2

--- scripts/servo_gpr/servo_gpr.ino
+++ scripts/servo_gpr/servo_gpr.ino
@@ -1,24 +1,24 @@
 
-#define LIN_ACT_1 5
-#define LIN_ACT_2 4
-#define LIN_ACT_PWM 3
-#include <Servo.h>
-
-#define SERVO_PIN 6          // PWM pin for Parallax CR servo
-
-Servo driveServo;
-
-void setServoRPM(int rpm) {
-  // Parallax CR-servo: ≈50 RPM @ 2 ms, ‑50 RPM @ 1 ms, stop @ 1.5 ms
-  const int DEADBAND_RPM = 2; // below this treat as stop
-  rpm = constrain(rpm, -50, 50);
-  if (abs(rpm) < DEADBAND_RPM) rpm = 0;
-
-  rpm = -rpm;  // Invert direction to match desired rotation
-
-  int pulse = 1500 + rpm * 8;   // 10 µs per RPM (500 µs / 50 RPM)
-  driveServo.writeMicroseconds(pulse);
-}
+#define LIN_ACT_1 9
+#define LIN_ACT_2 8
+#define LIN_ACT_PWM 10
+//#include <Servo.h>
+//
+//#define SERVO_PIN 6          // PWM pin for Parallax CR servo
+//
+//Servo driveServo;
+//
+//void setServoRPM(int rpm) {
+//  // Parallax CR-servo: ≈50 RPM @ 2 ms, ‑50 RPM @ 1 ms, stop @ 1.5 ms
+//  const int DEADBAND_RPM = 2; // below this treat as stop
+//  rpm = constrain(rpm, -50, 50);
+//  if (abs(rpm) < DEADBAND_RPM) rpm = 0;
+//
+//  rpm = -rpm;  // Invert direction to match desired rotation
+//
+//  int pulse = 1500 + rpm * 8;   // 10 µs per RPM (500 µs / 50 RPM)
+//  driveServo.writeMicroseconds(pulse);
+//}
 
 void gprPower() {
   analogWrite(LIN_ACT_PWM, 150);
@@ -81,8 +81,8 @@ void setup() {
   pinMode(LIN_ACT_PWM, OUTPUT); 
 
   // Attach continuous-rotation servo
-  driveServo.attach(SERVO_PIN);
-  setServoRPM(0);   // ensure stopped
+//  driveServo.attach(SERVO_PIN);
+//  setServoRPM(0);   // ensure stopped
 
   // Power on the GPR at startup
   gprPower();
@@ -98,12 +98,12 @@ void loop() {
 
     } else if (inputChar == 'K') {
       linestop();
-      setServoRPM(0);   // stop servo when scanning stops
+//      setServoRPM(0);   // stop servo when scanning stops
 
-    } else if (inputChar == 'S') {
-      // Expect ASCII signed integer RPM following 'S', terminated by \n
-      int rpm = Serial.parseInt();
-      setServoRPM(rpm);
+//    } else if (inputChar == 'S') {
+//      // Expect ASCII signed integer RPM following 'S', terminated by \n
+//      int rpm = Serial.parseInt();
+//      setServoRPM(rpm);
     }
   }
 }
