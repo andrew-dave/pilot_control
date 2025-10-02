0fbe6493c244509b168de67b01012dacc0415b6f
andrew-dave <evadaven.andrew@gmail.com>
2025-10-02 16:42:57 -0400

    fastlio inst vel publishing for plot viz

--- src/diff_drive_controller.cpp
+++ src/diff_drive_controller.cpp
@@ -10,6 +10,7 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/msg/transform_stamped.hpp>
+#include <deque>
 
 #include <signal.h>
 #include <atomic>
