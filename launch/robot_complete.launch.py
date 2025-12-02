efd34474fee429a27a159d84467cd427dc4009c0
RajasundaramM <rajasundaram18042001@gmail.com>
2025-12-02 17:23:08 -0500

    Re-tuning after bent chassis inc R_delta

--- launch/robot_complete.launch.py
+++ launch/robot_complete.launch.py
@@ -392,7 +392,7 @@ def generate_launch_description():
             'mpc_Q_xe': 15.0,
             'mpc_Q_ye': 20.0,
             'mpc_Q_yaw': 5.0,
-            'mpc_R_delta': 0.00010,
+            'mpc_R_delta': 0.00015,
             'mpc_weight_increase_xe': 0.0,
             'mpc_weight_increase_ye': 0.1,
             'mpc_weight_increase_yaw': 0.0,
