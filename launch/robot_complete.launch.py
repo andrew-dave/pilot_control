e8038235089eebb46404d78ab06a12ffc6923fd0
RajasundaramM <rajasundaram18042001@gmail.com>
2025-12-02 18:15:51 -0500

    Wheel radius corrected in complete launch

--- launch/robot_complete.launch.py
+++ launch/robot_complete.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     # Declare launch arguments
     declare_wheel_radius_arg = DeclareLaunchArgument(
         'wheel_radius',
-        default_value='0.072',
+        default_value='0.09',
         description='Radius of the wheels in meters.'
     )
     declare_wheel_base_arg = DeclareLaunchArgument(
@@ -392,7 +392,7 @@ def generate_launch_description():
             'mpc_Q_xe': 15.0,
             'mpc_Q_ye': 20.0,
             'mpc_Q_yaw': 5.0,
-            'mpc_R_delta': 0.00015,
+            'mpc_R_delta': 0.00010,
             'mpc_weight_increase_xe': 0.0,
             'mpc_weight_increase_ye': 0.1,
             'mpc_weight_increase_yaw': 0.0,
