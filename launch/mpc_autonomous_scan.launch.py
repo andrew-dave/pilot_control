f3c22ffb1cfcb89efaa9044e64f0cdb397c709ae
RajasundaramM <rajasundaram18042001@gmail.com>
2025-12-04 17:43:15 -0500

    New tuning

--- launch/mpc_autonomous_scan.launch.py
+++ launch/mpc_autonomous_scan.launch.py
@@ -74,27 +74,27 @@ def generate_launch_description():
     )
     declare_mpc_Q_xe_arg = DeclareLaunchArgument(
         'mpc_Q_xe',
-        default_value='5.0',
+        default_value='20.0',
         description='MPC cost weight for position error x'
     )
     declare_mpc_Q_ye_arg = DeclareLaunchArgument(
         'mpc_Q_ye',
-        default_value='20.0',
+        default_value='15.0',
         description='MPC cost weight for position error y (higher for lateral correction)'
     )
     declare_mpc_Q_yaw_arg = DeclareLaunchArgument(
         'mpc_Q_yaw',
-        default_value='10.0',
+        default_value='5.0',
         description='MPC cost weight for yaw error (higher to keep robot facing along the path and avoid backwards tracking)'
     )
     declare_mpc_R_delta_v_arg = DeclareLaunchArgument(
         'mpc_R_delta_v',
-        default_value='0.0080',
+        default_value='0.058',
         description='MPC cost weight for change in linear velocity (delta v)'
     )
     declare_mpc_R_delta_omega_arg = DeclareLaunchArgument(
         'mpc_R_delta_omega',
-        default_value='0.010',
+        default_value='0.0015',
         description='MPC cost weight for change in angular velocity (delta omega)'
     )
     # Error-reference lookahead shaping near start of line
@@ -115,7 +115,7 @@ def generate_launch_description():
     )
     declare_mpc_weight_increase_ye_arg = DeclareLaunchArgument(
         'mpc_weight_increase_ye',
-        default_value='0.2',
+        default_value='0.1',
         description='Linear weight increase factor per time step for ye error. Weight at step k = base_weight * (1 + weight_increase_ye * k). Example: 0.1 means 10% increase per step.'
     )
     declare_mpc_weight_increase_yaw_arg = DeclareLaunchArgument(
