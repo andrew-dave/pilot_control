1f8d647ff39ec44abe94f4f76ac20e1e6ab63067
RajasundaramM <rajasundaram18042001@gmail.com>
2025-12-03 20:57:58 -0500

    higher qxe test

--- launch/robot_complete.launch.py
+++ launch/robot_complete.launch.py
@@ -389,7 +389,7 @@ def generate_launch_description():
             # MPC parameters
             'mpc_horizon': 50,
             'mpc_dt': 0.1,
-            'mpc_Q_xe': 20.0,
+            'mpc_Q_xe': 30.0,
             'mpc_Q_ye': 15.0,
             'mpc_Q_yaw': 5.0,
             'mpc_R_delta': 0.00015,
