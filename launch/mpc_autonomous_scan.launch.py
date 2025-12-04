2fb81a550dc5bc92dcbb7481c9ad7cbeef071776
RajasundaramM <rajasundaram18042001@gmail.com>
2025-12-04 16:54:34 -0500

    New tuning

--- launch/mpc_autonomous_scan.launch.py
+++ launch/mpc_autonomous_scan.launch.py
@@ -79,12 +79,12 @@ def generate_launch_description():
     )
     declare_mpc_Q_ye_arg = DeclareLaunchArgument(
         'mpc_Q_ye',
-        default_value='20.0',
+        default_value='30.0',
         description='MPC cost weight for position error y (higher for lateral correction)'
     )
     declare_mpc_Q_yaw_arg = DeclareLaunchArgument(
         'mpc_Q_yaw',
-        default_value='5.0',
+        default_value='15.0',
         description='MPC cost weight for yaw error (higher to keep robot facing along the path and avoid backwards tracking)'
     )
     declare_mpc_R_delta_v_arg = DeclareLaunchArgument(
