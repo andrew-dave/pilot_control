57b5d6e9e288c31e13889c5a8aac7086620f3e45
andrew-dave <evadaven.andrew@gmail.com>
2025-12-05 20:04:02 -0500

    Flipped right camera stream

--- src/unified_data_collector.cpp
+++ src/unified_data_collector.cpp
@@ -937,10 +937,12 @@ private:
     oss << "! tee name=T_right ";
     
     // RIGHT stream branch (only if streaming right)
+    // Note: Right camera is mounted upside down, so we rotate 180 degrees for streaming
     if (stream_right) {
       oss << " T_right. ! queue leaky=downstream max-size-buffers=120 max-size-bytes=0 max-size-time=0 "
           << "! videorate ! video/x-raw,framerate=15/1 "
           << "! videoscale ! video/x-raw,width=640,height=480,format=I420 "
+          << "! videoflip method=rotate-180 "  // Rotate 180° for upside-down camera
           << "! x264enc tune=zerolatency speed-preset=ultrafast bitrate=" << cfg_.stream_bitrate_kbps << " key-int-max=30 bframes=0 "
           << "! video/x-h264,stream-format=byte-stream,alignment=au "
           << "! rtph264pay pt=96 config-interval=1 mtu=" << cfg_.rtp_mtu << " "
