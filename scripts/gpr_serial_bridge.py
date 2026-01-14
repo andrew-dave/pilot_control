ce25b2977ffb46cde392a4c2fe16d149683fc1a4
andrew-dave <evadaven.andrew@gmail.com>
2026-01-14 16:56:57 -0500

    Fix indentation bug in gpr_serial_bridge.py line_start_callback

--- scripts/gpr_serial_bridge.py
+++ scripts/gpr_serial_bridge.py
@@ -79,9 +79,9 @@ class GPRSerialBridge(Node):
             time.sleep(1)
             second_success = self.send_command('K')
             if second_success:
-            response.success = True
+                response.success = True
                 response.message = 'Line start commands (K, K) sent to Arduino'
-            self.scan_active = True
+                self.scan_active = True
             else:
                 response.success = False
                 response.message = 'Failed to send second K command'
@@ -116,4 +116,4 @@ def main(args=None):
         rclpy.shutdown()
 
 if __name__ == '__main__':
-    main() 
+    main()
